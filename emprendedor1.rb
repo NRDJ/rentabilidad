84f551677df8c488edf1a0c255dcb54cdc4b7a04
Nicolas Riedel <nriedeldj@gmail.com>
2020-08-20 13:49:32 -0400

    Added taxes to utilities

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -4,6 +4,10 @@ precio_venta = ARGV[0].to_i
 usuarios = ARGV[1].to_i
 gastos = ARGV[2].to_i
 
-utilidades = (precio_venta * usuarios) - gastos
+utilidades = ((precio_venta * usuarios) - gastos)
+
+if utilidades > 0
+    utilidades = utilidades * 0.65
+end
 
 puts utilidades
